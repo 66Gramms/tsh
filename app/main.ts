c18e3373f27be7a9a139b278387b34f18ff25dfc
66Gramms <meszarosricsi01@gmail.com>
2025-02-16 16:42:21 +0100

    Add type

--- app/main.ts
+++ app/main.ts
@@ -6,15 +6,25 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
+enum builtIns {
+  echo = "echo",
+  type = "type",
+  exit = "exit",
+}
+
 // Uncomment this block to pass the first stage
 rl.prompt();
 
 rl.on("line", (input) => {
-  if (input.startsWith("exit")) {
+  if (input.startsWith(builtIns.exit)) {
     rl.close();
     process.exit(0);
-  } else if (input.startsWith("echo ")) {
+  } else if (input.startsWith(builtIns.echo)) {
     console.log(input.slice(5));
+  } else if (input.startsWith(builtIns.type)) {
+    if (input.slice(5) in builtIns) {
+      console.log(`${input.slice(5)} is a shell built-in`);
+    }
   } else {
     console.log(`${input}: command not found`);
   }
