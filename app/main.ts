1bb5c62e1b6aacd984861b7416476fb460cf7359
66Gramms <meszarosricsi01@gmail.com>
2025-02-16 16:38:15 +0100

    Add echo

--- app/main.ts
+++ app/main.ts
@@ -13,7 +13,11 @@ rl.on("line", (input) => {
   if (input.startsWith("exit")) {
     rl.close();
     process.exit(0);
+  } else if (input.startsWith("echo ")) {
+    console.log(input.slice(5));
+  } else {
+    console.log(`${input}: command not found`);
   }
-  console.log(`${input}: command not found`);
+
   rl.prompt();
 });
